f5fc52c6cf4ff2d6480640d31dcade7230018fa6
rael <rael06@hotmail.fr>
2019-08-21 23:20:08 +0200

    read and write file exercise done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,47 +5,15 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
-        StringBuilder string = new StringBuilder();
-
-        if (args.length == 0) {
-            System.out.println("Veuillez renseigner le fichier dans les paramètres d'application en première position !");
-        } else {
-            try {
-                FileReader fichier = new FileReader(args[0]);
-                int octet = 0;
-                do {
-                    octet = fichier.read();
-                    if (octet > 0) {
-                        string.append((char) octet);
-                    }
-                } while (octet > 0);
-                System.out.println("Fin du fichier, on ferme !");
-                fichier.close();
-            } catch (FileNotFoundException e) {
-                System.err.println("Le fichier " + args[0] + " n'a pas ete trouve");
-            } catch (IOException e) {
-                System.err.println(e.getMessage());
-            }
-//            for (int i =0; i < string.length(); i++) {
-//                System.out.print(string.charAt(i));
-//            }
-        }
+        Methods methods = new Methods();
 
         if (args.length < 2) {
-            System.out.println("Veuillez renseigner le fichier dans les paramètres d'application en seconde position !");
+            System.out.println("Veuillez renseigner les fichiers dans les paramètres d'application !");
         } else {
-            try {
-                FileWriter copy = new FileWriter(args[1]);
-                for (int i =0; i < string.length(); i++) {
-                    copy.write(string.charAt(i));
-                }
-                System.out.println ("On ferme le fichier !");
-                copy.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            String message = "Je suis le texte copié !";
+            methods.writeReadAndCopy(args[0], "copie2.txt", message);
+            methods.copy(args[0], args[1]);
+            methods.readThenCopy(args[0], args[1]);
         }
-
-
     }
 }
---
+++ src/com/company/Methods.java
@@ -0,0 +1,79 @@
+package com.company;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Methods {
+
+    public void writeReadAndCopy(String _originFile, String _destinationFile, String _message) {
+        String message = _message != null ? _message : "Bonjour je teste l'écriture dans un fichier ! ";
+        try {
+            FileReader originFile = new FileReader(_originFile);
+            FileWriter destinationFile = new FileWriter(_destinationFile);
+            int octet = 0;
+
+            for (int i = 0; i < message.length(); i++) {
+                destinationFile.write(message.charAt(i));
+            }
+
+            destinationFile.write("\n");
+
+            do {
+                octet = originFile.read();
+                if (octet > 0) {
+                    destinationFile.write((char) octet);
+                }
+            } while (octet > 0);
+            originFile.close();
+            destinationFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void copy(String _originFile, String _destinationFile) {
+        try {
+            FileReader originFile = new FileReader(_originFile);
+            FileWriter copyFile = new FileWriter(_destinationFile);
+            int octet = 0;
+            do {
+                octet = originFile.read();
+                if (octet > 0) {
+                    copyFile.write((char) octet);
+                }
+            } while (octet > 0);
+            originFile.close();
+            copyFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readThenCopy(String _originFile, String _destinationFile) {
+
+        StringBuilder string = new StringBuilder();
+
+        try {
+            FileReader fichier = new FileReader(_originFile);
+            int octet = 0;
+            do {
+                octet = fichier.read();
+                if (octet > 0) {
+                    string.append((char) octet);
+                }
+            } while (octet > 0);
+            System.out.println("Fin de la copie du fichier d'origine, on ferme !");
+            fichier.close();
+
+            FileWriter copy = new FileWriter(_destinationFile);
+            for (int i = 0; i < string.length(); i++) {
+                copy.write(string.charAt(i));
+            }
+            System.out.println("Fin de l'écriture du fichier de destination, on ferme !");
+            copy.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
